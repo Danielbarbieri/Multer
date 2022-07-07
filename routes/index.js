49c2398877fe9edfde28e68f88bc8ca00105e121
Daniel Barbieri <danielbarbierianalista@gmail.com>
2022-07-07 17:54:56 -0300

    inicio subir arquivos

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,25 @@ const indexController = require('../controllers/indexController');
 const multer = require('multer');
 
 /* GET home page. */
+
+const storage = multer.diskStorage({ 
+  destination: function (req, file, cb) { 
+     cb(null, './public/images/avatars'); 
+  }, 
+  filename: function (req, file, cb) { 
+     cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);  } 
+})
+
+const uploadFile = multer({ storage });
+
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
+
+
 router.get('/', indexController.viewCadastro);
 router.post('/cadastro', indexController.salvarCadastro);
+
+router.post('/register', uploadFile.single('avatar'), usersController.create);
 
 module.exports = router;
